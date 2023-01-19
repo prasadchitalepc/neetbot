c0166580bd419a02726e67be89a95afec0d74ca1
prasadchitalepc <120631210+prasadchitalepc@users.noreply.github.com>
2023-01-19 16:10:35 +0530

    3 files modified

--- src/routes/sqa/+page.svelte
+++ src/routes/sqa/+page.svelte
@@ -11,7 +11,7 @@ import Card, {
   } from '@smui/card';
 
     import TopAppBar, { Row, Section, Title } from '@smui/top-app-bar';
-  
+  import IconButton from '@smui/icon-button';
  
   let prominent = false;
   let dense = false;
@@ -37,6 +37,7 @@ function handleShowAnswersClick() {
     toggle = true;
 	}
 </script>
+  
 <TopAppBar
       variant="static"
       {prominent}
@@ -46,19 +47,9 @@ function handleShowAnswersClick() {
       <Row>
         <Section>
           <IconButton class="material-icons">menu</IconButton>
-          <Title>Static</Title>
+          <Title>TakeUps</Title>
         </Section>
-        <Section align="end" toolbar>
-          <IconButton class="material-icons" aria-label="Download"
-            >file_download</IconButton
-          >
-          <IconButton class="material-icons" aria-label="Print this page"
-            >print</IconButton
-          >
-          <IconButton class="material-icons" aria-label="Bookmark this page"
-            >bookmark</IconButton
-          >
-        </Section>
+        
       </Row>
     </TopAppBar>
 
@@ -77,7 +68,43 @@ function handleShowAnswersClick() {
 {/if}
 
 
+<style>
+  .top-app-bar-container {
+    max-width: 480px;
+    width: 100%;
+    height: 320px;
+    border: 1px solid
+      var(--mdc-theme-text-hint-on-background, rgba(0, 0, 0, 0.1));
+    margin: 0 18px 18px 0;
+    background-color: var(--mdc-theme-background, #fff);
 
+    overflow: auto;
+    display: inline-block;
+  }
+
+  @media (max-width: 480px) {
+    .top-app-bar-container {
+      margin-right: 0;
+    }
+  }
+
+  .flexy {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  .flexor {
+    display: inline-flex;
+    flex-direction: column;
+  }
+
+  .flexor-content {
+    flex-basis: 0;
+    height: 0;
+    flex-grow: 1;
+    overflow: auto;
+  }
+</style>
 
 
 
