ee43022e5b5837307915d5d8a62c33ac787b5913
prasadchitalepc <120631210+prasadchitalepc@users.noreply.github.com>
2023-01-19 17:57:21 +0530

    1 file modified

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,125 +2,56 @@
 	<title>About</title>
 	<meta name="description" content="About this app" />
 </svelte:head>
+<TopAppBar bind:this={topAppBar} variant="fixed">
+  <Row>
+    <Section>
+      <IconButton class="material-icons">menu</IconButton>
+      <Title>Fixed</Title>
+    </Section>
+    <Section align="end" toolbar>
+      <IconButton class="material-icons" aria-label="Download"
+        >file_download</IconButton
+      >
+      <IconButton class="material-icons" aria-label="Print this page"
+        >print</IconButton
+      >
+      <IconButton class="material-icons" aria-label="Bookmark this page"
+        >bookmark</IconButton
+      >
+    </Section>
+  </Row>
+</TopAppBar>
+<AutoAdjust {topAppBar}>
 
+  <img
+    alt="Page content placeholder"
+    src="/page-content.jpg"
+    style="display: block; max-width: 100%; height: auto; margin: 1em auto;"
+  />
+</AutoAdjust>
 
-
-<div class="flexy">
-  <div class="top-app-bar-container flexor">
-    <TopAppBar
-      variant="static"
-      {prominent}
-      {dense}
-      color={secondaryColor ? 'secondary' : 'primary'}
-    >
-      <Row>
-        <Section>
-          <IconButton class="material-icons">menu</IconButton>
-          <Title>Flex Static</Title>
-        </Section>
-        <Section align="end" toolbar>
-          <IconButton class="material-icons" aria-label="Download"
-            >file_download</IconButton
-          >
-          <IconButton class="material-icons" aria-label="Print this page"
-            >print</IconButton
-          >
-          <IconButton class="material-icons" aria-label="Bookmark this page"
-            >bookmark</IconButton
-          >
-        </Section>
-      </Row>
-    </TopAppBar>
-    <div class="flexor-content">
-      
-      <img
-        alt="Page content placeholder"
-        src="/page-content.jpg"
-        style="display: block; max-width: 100%; height: auto; margin: 1em auto;"
-      />
-    </div>
-  </div>
-
-  <div class="top-app-bar-container">
-    <TopAppBar
-      variant="static"
-      {prominent}
-      {dense}
-      color={secondaryColor ? 'secondary' : 'primary'}
-    >
-      <Row>
-        <Section>
-          <IconButton class="material-icons">menu</IconButton>
-          <Title>Static</Title>
-        </Section>
-        <Section align="end" toolbar>
-          <IconButton class="material-icons" aria-label="Download"
-            >file_download</IconButton
-          >
-          <IconButton class="material-icons" aria-label="Print this page"
-            >print</IconButton
-          >
-          <IconButton class="material-icons" aria-label="Bookmark this page"
-            >bookmark</IconButton
-          >
-        </Section>
-      </Row>
-    </TopAppBar>
-    <div>
-      
-      <img
-        alt="Page content placeholder"
-        src="/page-content.jpg"
-        style="display: block; max-width: 100%; height: auto; margin: 1em auto;"
-      />
-    </div>
-  </div>
-</div>
-
-<script >
-  import TopAppBar, { Row, Section, Title } from '@smui/top-app-bar';
+<script lang="ts">
+  import TopAppBar, {
+    Row,
+    Section,
+    Title,
+    AutoAdjust,
+  } from '@smui/top-app-bar';
   import IconButton from '@smui/icon-button';
   
-
-  let prominent = false;
-  let dense = false;
-  let secondaryColor = false;
+  let topAppBar: TopAppBar;
 </script>
 
 <style>
-  .top-app-bar-container {
-    max-width: 480px;
-    width: 100%;
-    height: 320px;
-    border: 1px solid
-      var(--mdc-theme-text-hint-on-background, rgba(0, 0, 0, 0.1));
-    margin: 0 18px 18px 0;
-    background-color: var(--mdc-theme-background, #fff);
-
-    overflow: auto;
-    display: inline-block;
-  }
-
-  @media (max-width: 480px) {
-    .top-app-bar-container {
-      margin-right: 0;
-    }
-  }
-
-  .flexy {
-    display: flex;
-    flex-wrap: wrap;
-  }
-
-  .flexor {
-    display: inline-flex;
-    flex-direction: column;
-  }
-
-  .flexor-content {
-    flex-basis: 0;
-    height: 0;
-    flex-grow: 1;
-    overflow: auto;
+  /* Hide everything above this component. */
+  :global(#smui-app),
+  :global(body),
+  :global(html) {
+    display: block !important;
+    height: auto !important;
+    width: auto !important;
+    position: static !important;
   }
 </style>
+
+
