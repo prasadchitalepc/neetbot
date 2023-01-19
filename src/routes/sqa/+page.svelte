9d1ad4528b7d02dde23b2adcc11bde32f9ed812f
prasadchitalepc <120631210+prasadchitalepc@users.noreply.github.com>
2023-01-19 15:42:38 +0530

    1 file modified

--- src/routes/sqa/+page.svelte
+++ src/routes/sqa/+page.svelte
@@ -9,6 +9,13 @@ import Card, {
     ActionButtons,
     ActionIcons,
   } from '@smui/card';
+
+    import TopAppBar, { Row, Section, Title } from '@smui/top-app-bar';
+  
+ 
+  let prominent = false;
+  let dense = false;
+  let secondaryColor = false;
   var toggle = false;
 
   var qlst = {
@@ -30,6 +37,32 @@ function handleShowAnswersClick() {
     toggle = true;
 	}
 </script>
+<TopAppBar
+      variant="static"
+      {prominent}
+      {dense}
+      color={secondaryColor ? 'secondary' : 'primary'}
+    >
+      <Row>
+        <Section>
+          <IconButton class="material-icons">menu</IconButton>
+          <Title>Static</Title>
+        </Section>
+        <Section align="end" toolbar>
+          <IconButton class="material-icons" aria-label="Download"
+            >file_download</IconButton
+          >
+          <IconButton class="material-icons" aria-label="Print this page"
+            >print</IconButton
+          >
+          <IconButton class="material-icons" aria-label="Bookmark this page"
+            >bookmark</IconButton
+          >
+        </Section>
+      </Row>
+    </TopAppBar>
+
+
 {#if toggle}
 	<ShowAnswers questions={qlst}/>
   {:else}
