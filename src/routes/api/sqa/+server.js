b3c871de382169b667511a33df5885734a242cc7
prasadchitalepc <120631210+prasadchitalepc@users.noreply.github.com>
2023-01-25 16:37:32 +0530

    1 file modified

--- src/routes/api/sqa/+server.js
+++ src/routes/api/sqa/+server.js
@@ -4,7 +4,7 @@ import { json } from '@sveltejs/kit';
 import { getQuesAns } from "./chatgpt.js";
 export async function POST({ request }) {  
   var out = await getQuesAns();
-  console.log(out);
+  console.log(JSON.stringify(out));
   return new Response(JSON.stringify(out));
 
 }
